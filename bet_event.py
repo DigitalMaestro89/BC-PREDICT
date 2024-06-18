7232b473d1976f31c3d5c0bd5ad9a27d7228cb89
DigitalMaestro89 <johnnybrown98219@gmail.com>
2024-06-17 18:56:50 -0700

    Updated the bet_event.py to get the crash history data to use predict next crach value

--- bet_event.py
+++ bet_event.py
@@ -2,6 +2,8 @@ import subprocess
 from datetime import datetime
 import json
 import time
+import hmac
+import hashlib
 
 def get_history():
     batch_file_path = "site.bat"
@@ -22,29 +24,64 @@ def get_history():
             new_item = {
                 "gameId" : item["gameId"],
                 "hash" : game_detail["hash"],
-                "crash" : game_detail['rate']
+                "crash" : game_detail['rate'],
+                "salt" : game_detail['salt']
             }
             history_array.append(new_item)
         return history_array[0]
     except subprocess.CalledProcessError as e:
         print(f"Error executing batch file: {e}")
+
+# get crash value from its hash value
+def get_crash_from_hash(hash, salt):
+	hash = hmac.new(salt.encode(), bytes.fromhex(hash), hashlib.sha256).hexdigest()
+	n_bits = 52
+	r = int(hash[:n_bits // 4], 16)
+	X = r / (2 ** n_bits)
+	X = float(f'{X:.9f}')
+	X = 99 / (1 - X)
+	result = int(X)
+	return max(1, result / 100)
+
+# get precious hash value from next hash
+def get_previous_hash(hash):
+	return hashlib.sha256(hash.encode()).hexdigest()
+
+# get latest 200 crash history for predicting
+def get_latest_history(latest_hash, game_id, salt):
+    end_point = game_id - 200
+    # get crash history
+    crash_history = []
+    while game_id > end_point:
+       crash = get_crash_from_hash(latest_hash, salt)
+       crash_history.append(crash)
+       game_id -= 1
+       latest_hash = get_previous_hash(latest_hash)
+    # reverse the history array
+    crash_history = crash_history[::-1]
+    return crash_history
+
+# execute when the crash event occured
+def auto_bet(latest_hash, game_id, salt):
+    # get latest 200 crash values
+    history = get_latest_history(latest_hash, game_id, salt)
     
+    return 0
 
 def main() :
+    # get the latest crash event
     latest_bet = get_history()
     print(latest_bet)
+    # watch when the next crash event occur
     while True:
-        start_time = time.time()
         first_item = get_history()
-        end_time = time.time()
-        execution_time = end_time - start_time
         print(first_item)
         if latest_bet['gameId'] != first_item['gameId']:
             latest_bet = first_item
             print("Crash event occured")
-            print(f"Time: {execution_time}")
-            # print(latest_bet)
             print(f"Latest crash value: {latest_bet['crash']}")
+            auto_bet(latest_bet['hash'], latest_bet['gameId'], latest_bet['salt'])            
+
 
 if __name__ =="__main__":
     main()
